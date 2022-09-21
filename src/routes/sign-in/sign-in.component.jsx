3af60113601980287fd22e7b9ff24a7786acab69
Sashankr <sashankr97@gmail.com>
2022-09-21 12:46:27 +0530

    Added user to firebase collection

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,9 +1,10 @@
 import { signInWithGooglePopup } from "../../utils/firebase/firebase.util";
+import { createUserDocumentFromAuth } from "../../utils/firebase/firebase.util";
 
 const SignIn = () => {
   const googleSignInPopup = async () => {
-    const response = await signInWithGooglePopup();
-    console.log(response);
+    const { user } = await signInWithGooglePopup();
+    createUserDocumentFromAuth(user);
   };
 
   return (
